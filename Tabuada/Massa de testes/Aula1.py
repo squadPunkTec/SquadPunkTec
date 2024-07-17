a3948cc35dbbe9b857c4bdd3368cef41b2966e08
Mizael03 <mizaelsilvadeoliveira03@gmail.com>
2024-07-16 21:14:11 -0300

    fundamentos de automação

--- Tabuada/Massa de testes/Aula1.py
+++ Tabuada/Massa de testes/Aula1.py
@@ -1,17 +1,48 @@
-import pandas as pd
+from selenium.webdriver import Firefox
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+import time
 
-# Criando o DataFrame
-dados = {
-    'Nome': ['João', 'Maria', 'Pedro', 'Ana', 'José', 'Carla', 'Mariana'],
-    'Idade': [25, 30, 22, 28, 35, 27, 31],
-    'Cidade': ['Recife', 'Recife', 'Recife', 'Salvador', 'Salvador', 'São Paulo', 'Manaus']
-}
+browser = Firefox()
 
-df = pd.DataFrame(dados)
+link = 'https://google.com'
+browser.get(link)
 
-# Filtrando os moradores do Recife
-moradores_recife = df[df['Cidade'] == 'Recife']
+input_area = browser.find_element(By.NAME, "q")
+input_area.send_keys('Instituto Joga Junto')
+input_area.send_keys(Keys.ENTER)
 
-# Exibindo o DataFrame filtrado
-print("Moradores do Recife:")
-print(moradores_recife)
+time.sleep(10) 
+
+result_search = browser.find_elements(By.TAG_NAME, 'h3')
+
+check = False
+
+for result in result_search:
+    if 'Instituto Joga Junto' in result.text:
+        result.click()
+        print("Link encontrado e clicado!")
+        check = True
+        break
+
+title = browser.title
+assert 'Instituto Joga Junto' in title, "Página incorreta na busca"
+
+input_nome = browser.find_element(By.ID, "nome")
+input_nome.send_keys('André Alves de Lima')
+
+input_email = browser.find_element(By.ID, "email")
+input_email.send_keys('alveslimaandre@gmail.com')
+
+input_assunto = browser.find_element(By.ID, "assunto")
+select = Select(input_assunto)
+select.select_by_index(5)
+
+input_msg = browser.find_element(By.ID, "mensagem")
+input_msg.send_keys('Meu primeiro script de automação - Squad 6')
+
+button_enviar = browser.find_element(By.XPATH, "//button[@type='submit']")
+button_enviar.click()
+
+time.sleep(10)
